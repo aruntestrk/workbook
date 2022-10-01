07531890609ea24b56f08f15fd0fa79e443ba989
Arun R <aruntestrk@gmail.com>
2022-10-01 15:34:14 +1000

    Modified

--- PlanitSeleniumWork/src/test/java/com/plaint/selenium/pages/CartPage.java
+++ PlanitSeleniumWork/src/test/java/com/plaint/selenium/pages/CartPage.java
@@ -10,7 +10,6 @@ import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import com.planit.seleniium.base.BasePage;
 import com.planit.selenium.pojos.Product;
